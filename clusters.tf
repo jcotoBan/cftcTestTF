f262e8fa89dba184f81aa4593a6438fd3d553590
Juan Coto <jcotsan@gmail.com>
2023-08-20 20:07:36 -0600

    fixing var name referenced

--- clusters.tf
+++ clusters.tf
@@ -9,12 +9,12 @@ terraform {
 //Karmada manager cluster
 
 resource "linode_lke_cluster" "main_cluster" {
-    k8s_version = var.region_manager_lke_cluster[0].k8s_version
-    label = var.region_manager_lke_cluster[0].label
-    region = var.region_manager_lke_cluster[0].region
+    k8s_version = var.main_cluster[0].k8s_version
+    label = var.main_cluster[0].label
+    region = var.main_cluster[0].region
 
     dynamic "pool" {
-        for_each = var.region_manager_lke_cluster[0].pools
+        for_each = var.main_cluster[0].pools
         content {
             type  = pool.value["type"]
             count = pool.value["count"]
